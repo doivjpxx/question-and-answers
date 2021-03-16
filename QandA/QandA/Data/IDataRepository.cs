42a92cfbe5e474c3bfca1eabcbf551e05270099f
Huy Phong <phong.bnh@globalfactories.com>
2021-03-16 14:47:05 +0700

    first commit

---
+++ QandA/QandA/Data/IDataRepository.cs
@@ -0,0 +1,37 @@
+﻿using QandA.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearchWithPaging(string search, int pageNumber, int pageSize);
+
+        IEnumerable<QuestionGetManyResponse> GetQuestionsWithAnswers();
+
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+
+        IEnumerable<QuestionGetManyResponse> GetUnAnsweredQuestions();
+
+        Task<IEnumerable<QuestionGetManyResponse>> GetUnAnsweredQuestionsAsync();
+
+        QuestionGetSingleResponse GetQuestion(int questionId);
+
+        bool QuestionExists(int questionId);
+
+        AnswerGetResponse GetAnswer(int answerId);
+
+        QuestionGetSingleResponse PostQuestion(QuestionPostFullRequest question);
+
+        QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
+
+        void DeleteQuestion(int questionId);
+
+        AnswerGetResponse PostAnswer(AnswerPostFullRequest answer);
+    }
+}
---
+++ QandA/QandA/Data/Models/AnswerPostRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data.Models
+{
+    public class AnswerPostRequest
+    {
+        [Required]
+        public int? QuestionId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
